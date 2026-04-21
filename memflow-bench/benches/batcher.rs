0078d2c537af5544b9c26b8aadbdf818502a82c6
ko1N <ko1N1337@gmail.com>
2026-04-21 10:35:19 +0200

    Revert to global storage in batcher bench

--- memflow-bench/benches/batcher.rs
+++ memflow-bench/benches/batcher.rs
@@ -1,8 +1,9 @@
+#![allow(static_mut_refs)]
+
 use criterion::*;
 
 use memflow::prelude::v1::*;
 
-use std::cell::RefCell;
 use std::convert::TryInto;
 
 //use memflow::dummy::DummyMemory as Memory;
@@ -42,9 +43,7 @@ use rand::prelude::*;
 use rand::{Rng, SeedableRng};
 use rand_xorshift::XorShiftRng as CurRng;
 
-thread_local! {
-    static SCRATCH: RefCell<Vec<[u8; 16]>> = RefCell::new(vec![[0u8; 16]; 0x10000]);
-}
+static mut TSLICE: [[u8; 16]; 0x10000] = [[0; 16]; 0x10000];
 
 fn read_test_nobatcher<T: MemoryView>(
     chunk_size: usize,
@@ -71,18 +70,14 @@ fn read_test_nobatcher<T: MemoryView>(
 fn read_test_batcher<T: MemoryView>(chunk_size: usize, mem: &mut T, mut rng: CurRng, size: umem) {
     let base_addr = Address::from(rng.gen_range(0..size));
 
-    SCRATCH.with(|scratch| {
-        let mut scratch = scratch.borrow_mut();
+    let mut batcher = mem.batcher();
+    batcher.reserve(chunk_size);
 
-        let mut batcher = mem.batcher();
-        batcher.reserve(chunk_size);
+    for i in unsafe { TSLICE.iter_mut().take(chunk_size) } {
+        batcher.read_into(base_addr + rng.gen_range(0usize..0x2000), i);
+    }
 
-        for i in scratch.iter_mut().take(chunk_size) {
-            batcher.read_into(base_addr + rng.gen_range(0usize..0x2000), i);
-        }
-
-        let _ = black_box(batcher.commit_rw());
-    });
+    let _ = black_box(batcher.commit_rw());
 }
 
 fn read_test_with_ctx<T: MemoryView>(
@@ -95,13 +90,17 @@ fn read_test_with_ctx<T: MemoryView>(
 
     let mem_size = mem::mb(64);
 
-    let mut tslice = vec![[0u8; 16]; chunk_size];
     let mut tbuf = vec![];
 
     tbuf.extend(
-        tslice
+        unsafe { TSLICE }
             .iter_mut()
-            .map(|arr| CTup3(Address::INVALID, Address::INVALID, (&mut arr[..]).into())),
+            .map(|arr| {
+                CTup3(Address::INVALID, Address::INVALID, unsafe {
+                    std::mem::transmute::<&mut [u8], CSliceMut<'_, u8>>(&mut arr[..])
+                })
+            })
+            .take(chunk_size),
     );
 
     if !use_batcher {
